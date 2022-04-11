42ebc141f9ecb772a89716c0bb2d72426015a89e
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2022-04-11 16:48:52 +0100

    Moving deps in to their respective packages

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /\@nosferatu500/,
+            use: loaders.null(),
+          },
+        ],
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          stream: 'stream-browserify',
+          url: "url",
+        }),
+      ],
+      resolve: {
+        fallback: {
+          stream: 'stream',
+          url: "url",
+        }
+      },
+    })
+  } else {
+    actions.setWebpackConfig({
+      plugins: [
+        new webpack.ProvidePlugin({
+          stream: 'stream-browserify',
+          url: "url",
+        }),
+      ],
+      resolve: {
+        fallback: {
+          stream: 'stream-browserify',
+          url: "url",
+        }
+      },
+    })
+  }
+}
